c297fb6deccb4f7a0f8c16fa5da93faba5975b6a
MartinH <martin@hojman.com.ar>
2020-07-27 18:37:56 -0300

    Portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+let socializar = document.getElementById("socializar");
+socializar.onclick = function() {
+    window.open("https://github.com/MartinHoj/Trabajo-Integrador-DH", '_blank');
+};
+
+let ahorcado = document.getElementById("ahorcado");
+ahorcado.onclick = function() {
+    window.open("https://github.com/MartinHoj/pythonGames/tree/master/ahorcado", '_blank');
+};
